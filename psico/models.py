f5656bc84b2cc0e95bafc29c97ee6cc65cef3293
danisfon <danieledasilvafonseca@gmail.com>
2026-04-21 13:30:33 -0300

    fix | organizando projeto

--- psico/models.py
+++ psico/models.py
@@ -5,6 +5,6 @@ class Campus (models.Model) :
     nome = models.CharField(max_length=50)
     cadastrado_em = models.DateField(auto_now_add=True)
     atualizado_em = models.DateField(auto_now_add=True)
-    cadastrado_por = models.ForeignKey(User, on_delete=models.PROTECT)
+    #cadastrado_por = models.ForeignKey(User, on_delete=models.PROTECT)
 
     #COMANDO PARA CRIAR O O APP: python manage.py startapp NOME_AQUI
